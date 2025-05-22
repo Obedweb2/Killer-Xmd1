36ed0ed98e3a6796564a86f8d2b580b1dde2d82b
Obedweb2 <rongouniversity77@gmail.com>
2025-05-22 20:21:34 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -56,7 +56,7 @@ cmd({
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363416335506023@newsletter',
-          newsletterName: 'ObedTechX',
+          newsletterName: '🅾🅱🅴🅳🆃🅴🅲🅷',
           serverMessageId: 143
         }
       }
